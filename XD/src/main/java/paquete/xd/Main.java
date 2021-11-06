257e7e0c6f40410accc8605e4a0297110d4b966e
Kevin <kevinjair2003@gmail.com>
2021-11-05 21:17:59 -0600

    Token

--- XD/src/main/java/paquete/xd/Main.java
+++ XD/src/main/java/paquete/xd/Main.java
@@ -7,6 +7,7 @@ public class Main {
  
     public static void main(String[] args) {
         System.out.println("Holaaa");
+        System.out.println("Token: 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");
     }
     
 }
