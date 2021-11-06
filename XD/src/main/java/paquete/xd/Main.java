56e479812258ba92c7e7b8b683a620832612d9d3
Kevin <kevinjair2003@gmail.com>
2021-11-05 21:30:22 -0600

    TOKEN eclipse

--- XD/src/main/java/paquete/xd/Main.java
+++ XD/src/main/java/paquete/xd/Main.java
@@ -8,6 +8,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Holaaa");
         System.out.println("Token netBeans: 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");
+        System.out.println("Token Eclipse: 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");
         System.out.println("Esto es una prueba");
         System.out.println("Vamos a jugar lol");
     }
