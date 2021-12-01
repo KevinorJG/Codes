1b330b226e046f7c889b75bd99c877aa6bc6f884
Kevin <kevinjair2003@gmail.com>
2021-11-30 19:08:03 -0600

    Prueba xdxdsds

--- Practica/src/main/java/Main/MainPrincipal.java
+++ Practica/src/main/java/Main/MainPrincipal.java
@@ -14,7 +14,10 @@ public class MainPrincipal {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        int i = 2, j = 3;
+        
+        System.out.println("La suma es: "+(i+j));
+                
     }
     
 }
